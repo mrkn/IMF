a9a8e761dfd05f466409adca74e4236eb082693e
Kenta Murata <mrkn@mrkn.jp>
2016-05-06 21:59:57 +0900

    :heavy_plus_sign: Add IMF::ImageSource adapter class

---
+++ lib/IMF/image_source.rb
@@ -0,0 +1,92 @@
+module IMF
+  class ImageSource
+    INITIAL_BUFFER_SIZE = 8192
+
+    class InvalidSourceError < IMF::Error
+    end
+
+    def initialize(source)
+      @buffer = '\0' * INITIAL_BUFFER_SIZE
+      @read_size = 0
+      @pos = 0
+
+      case source
+      when String
+        init_with_path(source)
+      when IO, File
+        init_with_io(source)
+      else
+        init_with_readable(source)
+      end
+    end
+
+    def read(length = nil, outbuf = '')
+      init_with_io(File.open(@path, 'rb')) unless @source
+
+      if length.nil?
+        data = @source.read
+        @buffer[@read_size, data.length] = data
+        @read_size += data.length
+        beg, @pos = @pos, @read_size
+        outbuf.replace @buffer[beg..-1]
+      elsif length > 0
+        tail_length = @read_size - @pos
+        shortage_length = length - tail_length
+        if shortage_length <= 0
+          outbuf.replace @buffer[@pos, length]
+          @pos += length
+        else
+          data = @source.read(shortage_length)
+          @buffer[@read_size, data.length] = data
+          @read_size += data.length
+          beg, @pos = @pos, @read_size
+          outbuf.replace @buffer[beg, length]
+        end
+      end
+
+      outbuf
+    end
+
+    def rewind
+      @pos = 0
+    end
+
+    private
+
+    def init_with_path(path)
+      unless File.exist? path
+        raise Errno::ENOENT
+      end
+
+      if File.directory? path
+        raise Errno::EISDIR
+      end
+
+      unless File.readable? path
+        raise Errno::EPERM
+      end
+
+      @path = path
+      @source = nil
+    end
+
+    def init_with_io(io)
+      # check readability
+      begin
+        io.read_nonblock(0)
+      rescue SystemCallError
+        # ignored
+      end
+
+      @source = io
+    end
+
+    def init_with_readable(obj)
+      if obj.respond_to?(:read)
+        @source = obj
+      else
+        raise InvalidSourceError
+      end
+    end
+  end
+end
---
+++ spec/IMF/image_source_spec.rb
@@ -0,0 +1,135 @@
+require 'spec_helper'
+
+require 'stringio'
+require 'zlib'
+
+RSpec.describe IMF::ImageSource do
+  let(:image_path) do
+    fixture_file('momosan.jpg')
+  end
+
+  subject(:image_source) do
+    IMF::ImageSource.new(source)
+  end
+
+  shared_context 'image file source' do
+    let(:source) do
+      image_path
+    end
+  end
+
+  shared_context 'gzipped io source' do
+    let(:gzipped_source) do
+      StringIO.new.tap { |strio|
+        Zlib::GzipWriter.wrap(strio) do |gzio|
+          gzio.write(IO.read(image_path, mode: 'rb'))
+        end
+      }.string
+    end
+
+    let(:source) do
+      Zlib::GzipReader.wrap(StringIO.new(gzipped_source))
+    end
+  end
+
+  describe '#read' do
+    shared_examples 'Reading bytes' do
+      context 'When reading bytes separatedly' do
+        let(:read_lengths) do
+          [2, 3, 5, 7, 9, 11, 13]
+        end
+
+        let(:total_length) do
+          read_lengths.inject(:+)
+        end
+
+        let(:read_bytes) do
+          read_lengths.map { |length|
+            image_source.read(length)
+          }.join('')
+        end
+
+        let(:expected_bytes) do
+          IO.read(image_path, total_length, mode: 'rb')
+        end
+
+        it 'reads bytes correctly' do
+          expect(read_bytes).to eq(expected_bytes)
+        end
+      end
+    end
+
+    context 'Given image_source is initialized with a image file path' do
+      include_context 'image file source'
+
+      include_examples 'Reading bytes'
+    end
+
+    context 'Given image_source is initialized with an IO object' do
+      context 'When the IO object is readable' do
+        let(:source) do
+          open(image_path, 'rb')
+        end
+
+        include_examples 'Reading bytes'
+      end
+
+      context 'When the IO object is not readable', :run_in_tmpdir do
+        let(:tmpfile_path) do
+          File.expand_path(File.basename(image_path), tmpdir)
+        end
+
+        before do
+          IO.write(tmpfile_path, IO.read(image_path, mode: 'rb'), mode: 'wb')
+        end
+
+        let(:source) do
+          open(tmpfile_path, 'wb')
+        end
+
+        it do
+          expect { image_source.read(1) }.to raise_error(IOError)
+        end
+      end
+    end
+
+    context 'Given image_source is initialized with a readable' do
+      context 'When the readable is a StringIO' do
+        let(:source) do
+          StringIO.new(IO.read(image_path, mode: 'rb'))
+        end
+
+        include_examples 'Reading bytes'
+      end
+
+      context 'the readable is a Zlib::GzipReader' do
+        include_context 'gzipped io source'
+
+        include_examples 'Reading bytes'
+      end
+    end
+  end
+
+  describe '#rewind' do
+    shared_examples 'rewind' do
+      it 'rewind the reading cursor to the beginning of file' do
+        lead_1024_bytes = IO.read(image_path, 1024, mode: 'rb')
+        image_source.read(128)
+        image_source.rewind
+        expect(image_source.read(1024)).to eq(lead_1024_bytes)
+      end
+    end
+
+    context 'Given image_source is initialized with a image file path' do
+      include_context 'image file source'
+
+      include_examples 'rewind'
+    end
+
+    context 'Given image_source is initialized with a Zlib::GzipReader object' do
+      include_context 'gzipped io source'
+
+      include_examples 'rewind'
+    end
+  end
+end
